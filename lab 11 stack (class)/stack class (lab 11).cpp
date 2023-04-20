6212b5470ef4c0241730c5e80d7d35788feb6cd4
Кирпичников Илья РИС-22-1б <48968239+kirpilya@users.noreply.github.com>
2023-04-20 22:03:41 +0500

    Add files via upload

---
+++ lab 11 stack (class)/stack class (lab 11).cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include "Stack.h"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    Stack st1(5, 1);
+    st1.print();
+
+    cout << endl << endl;
+
+    st1.push(10);
+    st1.push(20);
+    st1.print();
+
+    cout << endl << endl;
+
+    st1.pop();
+    st1.print();
+
+    cout << endl << endl;
+
+    st1.insert(3, 999);
+    st1.print();
+
+    cout << endl << endl;
+
+    st1.erase(999);
+    st1.print();
+
+    cout << endl << endl;
+}
