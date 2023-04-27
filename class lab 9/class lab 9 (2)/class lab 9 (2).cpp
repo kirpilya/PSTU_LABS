ff9971366249ec6a859542a3f09c99630ab64eb6
Кирпичников Илья РИС-22-1б <48968239+kirpilya@users.noreply.github.com>
2023-04-27 23:28:45 +0500

    Add files via upload

---
+++ class lab 9/class lab 9 (2)/class lab 9 (2).cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include "List.h"
+#include "Error.h"
+
+using namespace std;
+
+int main()
+{
+    // MAX_SIZE = 30
+
+    try
+    {
+        List a(40, 0);
+    }
+    catch (Error& err)
+    {
+        cout << err.what() << endl;
+    }
+
+    try
+    {
+        List a(30, 0);
+        a.push_back(5);
+
+    }
+    catch (Error& err)
+    {
+        cout << err.what() << endl;
+    }
+
+    try
+    {
+        List a;
+        a.pop_back();
+    }
+    catch (Error& err)
+    {
+        cout << err.what() << endl;
+    }
+
+    try
+    {
+        List a(20, 0);
+        a[-2] = 5;
+    }
+    catch (Error& err)
+    {
+        cout << err.what() << endl;
+    }
+
+    try
+    {
+        List a(20, 0);
+        a[25] = 5;
+    }
+    catch (Error& err)
+    {
+        cout << err.what() << endl;
+    }
+
+    try
+    {
+        List a(20, 0);
+        List b(20, 1);
+
+        a = a + b;
+    }
+    catch (Error& err)
+    {
+        cout << err.what() << endl;
+    }
+
+    try
+    {
+        List a(30, 0);
+        a + 5;
+    }
+    catch (Error& err)
+    {
+        cout << err.what() << endl;
+    }
+}
